c2da82a4e0e168da3abf187bed280cce21f255d7
Orion Railean <orion.railean@gmail.com>
2024-07-31 07:38:52 +1000

    add lines on percentage bar

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ class $modify(MyLevelCell, LevelCell) {
 
         const auto cache = DataCache::get();
         if (const auto data = cache->retrieve(m_level->m_levelID); data != nullptr) {
-            postDownload();
+            createBadge();
             return;
         }
 
@@ -59,8 +59,8 @@ class $modify(MyLevelCell, LevelCell) {
                     const auto expirationHeader = res->header("cache-control").value_or("max-age=0");
                     const auto expiration = expirationHeader.substr(expirationHeader.find("=") + 1);
                     Loader::get()->queueInMainThread([this, cache, data, expiration, timestamp] {
-                        cache->store(m_level->m_levelID, data, std::stol(expiration)+timestamp);
-                        postDownload();
+                        cache->store(m_level->m_levelID, data["data"], std::stol(expiration)+timestamp);
+                        createBadge();
                     });
                 } else {
                     release();
@@ -104,10 +104,6 @@ class $modify(MyLevelCell, LevelCell) {
         badge->setPosition(pos);
         badge->setScale(spriteSize);
         badge->release();
-    }
-
-    void postDownload() const {
-        createBadge();
     }
 };
 
@@ -154,8 +150,8 @@ class $modify(MyLevelInfoLayer, LevelInfoLayer) {
                     const auto expirationHeader = res->header("cache-control").value_or("max-age=0");
                     const auto expiration = expirationHeader.substr(expirationHeader.find("=") + 1);
                     Loader::get()->queueInMainThread([this, cache, data, expiration, timestamp] {
-                        cache->store(m_level->m_levelID, data, std::stol(expiration)+timestamp);
-                        postDownload(data);
+                        cache->store(m_level->m_levelID, data["data"], std::stol(expiration)+timestamp);
+                        postDownload(data["data"]);
                     });
                 } else {
                     release();
@@ -232,13 +228,10 @@ class $modify(MyPlayLayer, PlayLayer) {
     void drawRoutes(matjson::Value data) {
         const auto pb = this->getChildByID("progress-bar");
         if (!pb) return;
-        const auto pbSize = pb->getScaledContentSize();
+        const auto pbSize = pb->getScaledContentWidth();
         std::vector<Route> routes = {};
-        const std::string dump = data.dump(0);
-        Mod::get()->getConfigDir();
-        const auto rts = data["routes"].as_array();
 
-        for (const auto& route : rts) {
+        for (const auto rts = data["routes"].as_array(); const auto& route : rts) {
             routes.push_back(Route {
                 .start = route["start"].as_int(),
                 .end = route["end"].as_int(),
@@ -247,18 +240,24 @@ class $modify(MyPlayLayer, PlayLayer) {
             });
         }
 
-        for (auto i = 0; ++i;) {
-            const auto route = routes[i];
-            const auto start = route.start * (pbSize.width - 2.f) / 100.f;
-            const auto end = route.end * (pbSize.width - 2.f) / 100.f;
-            auto startLine = CCLayerColor::create(m_fields->colors[i], 2.f, 6.f);
+        float i = 0;
+        for (const auto& route : routes) {
+            const auto parent = CCNode::create();
+            parent->setID(std::format("route-{}"_spr, i));
+            pb->addChild(parent);
+            const auto start = route.start * (pbSize - 2.f) / 100.f;
+            const auto end = route.end * (pbSize - 2.f) / 100.f;
+            const auto startLine = CCLayerColor::create(m_fields->colors[i], 2.f, 6.f);
             startLine->setPosition(CCPoint(start, 5.f));
             startLine->setVisible(route.start > 1 && route.start < 99);
-            pb->addChild(startLine);
-            auto endLine = CCLayerColor::create(m_fields->colors[i], 2.f, 6.f);
+            startLine->setID("start-line"_spr);
+            parent->addChild(startLine);
+            const auto endLine = CCLayerColor::create(m_fields->colors[i], 2.f, 6.f);
             endLine->setPosition(CCPoint(end, 5.f));
-            startLine->setVisible(route.end > 1 && route.end < 99);
-            pb->addChild(endLine);
+            endLine->setVisible(route.end > 1 && route.end < 99);
+            endLine->setID("end-line"_spr);
+            parent->addChild(endLine);
+            ++i;
         }
     }
 };
